89576778f1ef1eefbdb61bfd71dc28e4c33ab72d
Fabiano Tozatti <fabianotozatti@gmail.com>
2021-03-25 20:30:35 -0300

    Conexao_V3

--- Back/conexao/conexao.go
+++ Back/conexao/conexao.go
@@ -3,16 +3,10 @@ package conexao
 import (
 	"database/sql"
 	"fmt"
-
-	/* importei com '_' pois é preciso inicializar a biblioteca, e evitar
-	   efeitos colaterais (como o fato de não chamá-la diretamente no seu
-	   código) */
-	_ "github.com/go-sql-driver/mysql"
 )
 
-// a ideia dessa função aqui é retornar uma forma de conexão com o banco
 func Connect() *sql.DB {
-	//  aqui você substitui as variáveis pelas suas configs
+
 	driverConfig := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", "sistema", "Dfffgl#2021", "127.0.0.1", "3306", "AgendaMedica")
 	connection, err := sql.Open("mysql", driverConfig)
 	if err != nil {
