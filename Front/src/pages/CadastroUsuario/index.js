c493a4c6857b867ab25ec1effa354a49023ae178
flaviamaturo <flaviacmaturo@gmail.com>
2021-06-16 12:09:03 -0300

    add campos tela usuario

--- Front/src/pages/CadastroUsuario/index.js
+++ Front/src/pages/CadastroUsuario/index.js
@@ -91,40 +91,60 @@ class User extends React.Component {
             <div style={{ padding: "26px" }}>
                 <form onSubmit={this.handleSubmit}>
                     <div class="form-row">
-                    <div class="form-group col-md-3">
-                        <label for="inputEmail4">Nome Completo</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="Nome Completo" onChange={this.handleChange}/>
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="inputPassword4">CPF</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="CPF" onChange={this.handleChange}/>
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="inputAddress">Telefone</label>
-                        <input type="text" class="form-control" id="inputAddress" placeholder="Telefone" onChange={this.handleChange}/>
-                    </div>
+                        <div class="form-group col-md-3">
+                            <label for="inputNome">Nome Completo</label>
+                            <input type="text" class="form-control" id="inputNome" placeholder="Nome Completo" onChange={this.handleChange}/>
+                        </div>
+                        <div class="form-group col-md-3">
+                            <label for="inputDatanasc">Data de Nascimento</label>
+                            <input type="text" class="form-control" id="inputDatanasc" placeholder="Data de nascimento" onChange={this.handleChange}/>
+                        </div>
+                        <div class="form-group col-md-3">
+                            <label for="inputCPF">CPF</label>
+                            <input type="text" class="form-control" id="inputCPF" placeholder="CPF" onChange={this.handleChange}/>
+                        </div>
+                        <div class="form-group col-md-3">
+                            <label for="inputTelefone">Telefone</label>
+                            <input type="text" class="form-control" id="inputTelefone" placeholder="Telefone" onChange={this.handleChange}/>
+                        </div>
                     </div>
                     
                     <div class="form-group">
-                    <label for="inputAddress2">Endereço</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartamento, hotel, casa, etc." onChange={this.handleChange}/>
+                        <label for="inputEmail">Email</label>
+                        <input type="email" class="form-control" id="inputEmail" placeholder="Email" onChange={this.handleChange}/>
                     </div>
+
+                    <div class="form-group">
+                        <label for="inputAddress">Endereço</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="Apartamento, hotel, casa, etc." onChange={this.handleChange}/>
+                    </div>
+
                     <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputCity">Cidade</label>
-                        <input type="text" class="form-control" id="inputCity" placeholder="..." onChange={this.handleChange}/>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="inputEstado">Estado</label>
-                        <select id="inputEstado" class="form-control" onChange={this.handleChange}>
-                        <option selected>Escolher...</option>
-                        <option>...</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-2">
-                        <label for="inputCEP">CEP</label>
-                        <input type="text" class="form-control" id="inputCEP" placeholder="..." onChange={this.handleChange}/>
-                    </div>
+                        <div class="form-group col-md-3">
+                            <label for="inputAddress2">Bairro</label>
+                            <input type="text" class="form-control" id="inputAddress2" placeholder="Bairro" onChange={this.handleChange}/>
+                        </div>
+                    
+                        <div class="form-group col-md-3">
+                            <label for="inputCity">Cidade</label>
+                            <input type="text" class="form-control" id="inputCity" placeholder="..." onChange={this.handleChange}/>
+                        </div>
+                        {/* <div class="form-group col-md-4">
+                            <label for="inputEstado">Estado</label>
+                            <select id="inputEstado" class="form-control" onChange={this.handleChange}>
+                            <option selected>Escolher...</option>
+                            <option>...</option>
+                            </select>
+                        </div> */}
+                        <div class="form-group col-md-3">
+                            <label for="inputCEP">CEP</label>
+                            <input type="text" class="form-control" id="inputCEP" placeholder="..." onChange={this.handleChange}/>
+                        </div>
+                        <div class="form-group col-md-3">
+                            <label for="inputSenha">Senha</label>
+                            <input type="password" class="form-control" id="inputSenha" placeholder="Senha" onChange={this.handleChange}/>
+                        </div>
+                        
                     </div>
                     <br/>
                     <br/>
