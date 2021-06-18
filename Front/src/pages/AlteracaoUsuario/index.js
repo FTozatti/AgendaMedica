84d0088135abb6de261eae0cb84b8ca7f74c2ec8
Laís de Paula <laisdepaula53@gmail.com>
2021-06-17 23:19:22 -0300

    update Alteração

--- Front/src/pages/AlteracaoUsuario/index.js
+++ Front/src/pages/AlteracaoUsuario/index.js
@@ -43,13 +43,6 @@ function AlterarUsuario() {
                     <label for="inputCity">Cidade</label>
                     <input type="text" class="form-control" id="inputCity" placeholder="..."/>
                 </div>
-                <div class="form-group col-md-4">
-                    <label for="inputEstado">Estado</label>
-                    <select id="inputEstado" class="form-control">
-                    <option selected>Escolher...</option>
-                    <option>...</option>
-                    </select>
-                </div>
                 <div class="form-group col-md-2">
                     <label for="inputCEP">CEP</label>
                     <input type="text" class="form-control" id="inputCEP" placeholder="..."/>
